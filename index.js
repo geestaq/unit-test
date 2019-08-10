80c1e9c4f63db28c094615b6720cafbcbf047f3e
geestaq <gee@vp.pl>
2019-08-10 11:02:36 +0200

    Change formatDate function

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const formatDate = (timeInSeconds) => {
-  return `${timeInSeconds}s`;
+  let seconds = timeInSeconds || 0;
+
+  //godziny
+  let hours = 0;
+  if(seconds > 3600) {
+    hours = parseInt(seconds / 3600);
+    seconds -= hours * 3600;
+  }
+  hours = (hours > 0) ? `${hours}h` : '';
+
+  //minuty
+  let minutes = 0;
+  if(seconds > 60) {
+    minutes = parseInt(seconds / 60);
+    seconds -= minutes * 60;
+  }
+  minutes = (minutes > 0) ? ` ${minutes}m` : '';
+
+  //sekundy
+  seconds = (seconds > 0
+    || (seconds + minutes + hours) == 0) ? ` ${seconds}s` : '';
+
+  return `${hours}${minutes}${seconds}`.trim();
+  //a
 }
 
 module.exports = formatDate;
